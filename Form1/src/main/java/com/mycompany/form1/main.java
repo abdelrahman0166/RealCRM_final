f263baa5f0e8cbe1e776e2fe0483c3d696d90236
abdelrahman0166 <58846234+abdelrahman0166@users.noreply.github.com>
2020-02-19 14:29:54 +0300

    Add files via upload

---
+++ Form1/src/main/java/com/mycompany/form1/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.form1;
+
+/**
+ *
+ * @author AbdelRahman Mohamed
+ */
+public class main {
+    public static void main(String[] args){
+    Signinform s = new Signinform();
+    s.setVisible(true);
+    }
+    
+}
